b737f449f16c634fa563cdb6f3e2a2daf16bc8a4
junggji <calrinar@gmail.com>
2023-10-31 15:49:12 +0900

    프로퍼티의 값은 자바스크립트 코드가 될 수 있음

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 function App() {
-  // JSX가 js 코드로 변환됨
-  // 따라서 js 키워드를 사용할 수 없음
-
   return (
     <>
-      {/* class 속성은 className으로 작성 */}
-      <div className="header">Lorem ipsum dolor.</div>
-      <div className="error">Lorem ipsum dolor.</div>
-      {/* for 속성은 htmlFor로 작성 */}
-      <div>
-        <label htmlFor="nameInput">이름</label>
-        <input type="text" id="nameInput" />
-      </div>
+      {" "}
+      <h1>이름은 {name}</h1>
+      <h1> 이름은 {"강인"}</h1>
+      <h1>이름은 희찬</h1>
+      <div className={"error"}> Lorem ipsum dolor sit.</div>
+      <div className="error">Lorem ipsum dolor sit.</div>
     </>
   );
 }
