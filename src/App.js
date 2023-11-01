3f6e53b6a7eb699262de8a9fa9286a825b0ee548
junggji <calrinar@gmail.com>
2023-11-01 12:21:56 +0900

    한 파일에서 여러개의 값(객체, 값, 함수) export, import 가능

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 // export된 컴포넌트(변수, 함수, 객체) 를 import 해서 사용
-import { MyElem } from "./component/MyElem";
-import { MyComp } from "./component/MyComp";
-import { MyHeader } from "./component/MyHeader";
+// 한 파일 안에서 여러 컴포넌트 가져올 수( 쓸 수) 있다.
+// 변수나 객체로 import해올수있다.
+
+import { MyBox, MyElem, MyContainer, name, address, person } from "./MyBox";
 
 function App() {
+  console.log("name", name);
+  console.log("address", address);
+  console.log("person", person);
   return (
     <>
-      <MyHeader />
-      <MyComp />
+      <MyBox />
       <MyElem />
+      <MyContainer />
     </>
   );
 }
--- src/component/MyHeader.js
+++
@@ -1,7 +0,0 @@
-export function MyHeader() {
-  return (
-    <div>
-      <p>넘나 긴내용</p>
-    </div>
-  );
-}
