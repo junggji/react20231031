a783eed2a79fae99baabd13b8628ffda1c41cf98
junggji <calrinar@gmail.com>
2023-11-01 15:48:03 +0900

    보통의 css 사용시 클래스명이 중복될 수 있음

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 // style 적용 방법
-// 보통 css 파일
+// 보통 css 파일 --> 같은 이름의 class사용될 경우
 // css module 사용
 // chakraui css library
 
 import { MyComp } from "./component/MyComp";
 import { MyBox } from "./component/MyBox";
+import { MyContainer } from "./component/MyContainer";
 
 function App() {
   return (
     <>
       <MyComp />
       <MyBox />
+      <MyContainer />
     </>
   );
 }
