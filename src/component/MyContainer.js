335e2dec1d078bb53e82d866db9ce3658e202938
junggji <calrinar@gmail.com>
2023-11-01 16:29:48 +0900

    css module을 사용할 때 여러 class를 부여하는 방법

--- src/component/MyContainer.js
+++ src/component/MyContainer.js
@@ -3,7 +3,14 @@ import style from "../style/style2.module.css";
 export function MyContainer() {
   return (
     <div>
-      <h1 className={style.boxClass}>Lorem ipsum dolor sit amet.</h1>
+      {/* css module에서 class여러개 줄때는 `` (template literal) 사용*/}
+      <h1 className={`${style.boxClass} ${style.note} ${style.error}`}>
+        Lorem ipsum dolor sit amet.
+      </h1>
+      {/* 2번째 방법 join(" ") */}
+      <h1 className={[style.boxClass, style.error, style.note].join(" ")}>
+        Lorem ipsum dolor sit amet.
+      </h1>
     </div>
   );
 }
