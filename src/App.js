75a68d8810d88aaaf7276845db6a68f446a6e1b5
junggji <calrinar@gmail.com>
2023-11-02 15:36:34 +0900

    Image 사용

--- src/App.js
+++ src/App.js
@@ -6,17 +6,41 @@ import {
   FormLabel,
   Switch,
   Textarea,
+  Image,
 } from "@chakra-ui/react";
 
 function App(props) {
   return (
     <div>
       <Box w={"480px"}>
-        <FormControl display={"flex"} alignItems={"center"}>
-          <FormLabel mb={0}>알림을 받으시겠습니까?</FormLabel>
-          <Switch size={"lg"} colorScheme="orange" />
-        </FormControl>
+        <Image src="https://bit.ly/dan-abramov" />
       </Box>
+      <Box>
+        <Image boxSize={"100px"} src="https://bit.ly/dan-abramov" />
+      </Box>
+      <Box>
+        <Image boxSize={"200px"} src="https://bit.ly/dan-abramov" />
+      </Box>
+      <Box>
+        <Image
+          borderRadius={"full"}
+          boxSize={"200px"}
+          src="https://bit.ly/dan-abramov"
+        />
+      </Box>
+      <Box>
+        <Image
+          boxSize={"100px"}
+          src="abcde.jpg"
+          fallbackSrc="https://via.placeholder.com/150"
+        />
+      </Box>
+      <Box></Box>
+      <Box></Box>
+      <Box></Box>
+      <Box></Box>
+      <Box></Box>
+      <Box></Box>
     </div>
   );
 }
