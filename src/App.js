5c054b6ee450612cdc8c7b29c72ac9281b36a70a
junggji <calrinar@gmail.com>
2023-11-02 15:20:11 +0900

    textarea 사용

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React from "react";
-import { Box, FormControl, Select } from "@chakra-ui/react";
+import { Box, FormControl, FormLabel, Textarea } from "@chakra-ui/react";
 
 function App(props) {
   return (
     <div>
       <Box w={"480px"}>
         <FormControl>
-          <Select placeholder="선택하세요" multiple h={"150px"}>
-            <option value="value1">Lorem.</option>
-            <option value="value2">Velit?</option>
-            <option value="value3">Eum.</option>
-            <option value="value4">Ullam.</option>
-            <option value="value5">Facere.</option>
-          </Select>
+          <FormLabel>본문</FormLabel>
+          <Textarea placeholder="본문을 작성하세요" h={"500px"}></Textarea>
         </FormControl>
       </Box>
     </div>
