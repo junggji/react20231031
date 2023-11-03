6c1042cc8c2b290601afa4b570737b8a95db90c2
junggji <calrinar@gmail.com>
2023-11-03 15:30:44 +0900

    예제) state를 가공해서 쓸 수 있음

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, { useState } from "react";
-import { Input, Text } from "@chakra-ui/react";
+import { Input, NumberInput, Text } from "@chakra-ui/react";
 
 function App(props) {
-  const [message, setMessage] = useState("");
+  const [number, setNumber] = useState(0);
+
+  // state를 가공해서 사용할 수 도있다.
+  const isOdd = number % 2 == 1;
 
   function handleInputChange(e) {
-    setMessage(e.target.value);
+    // e.target.value의 타입은 string이기 때문에 Number로 변환해서 연산할 수 있게함
+    setNumber(Number(e.target.value));
   }
   return (
     <div>
-      <Input onChange={handleInputChange} />
-      <Text>{message}</Text>
+      <Input type="number" onChange={handleInputChange} />
+      <Text>
+        {isOdd && "홀수"}
+        {isOdd || "짝수"}
+      </Text>
     </div>
   );
 }
