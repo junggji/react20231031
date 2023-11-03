9b7358d8a6669092e724b1a8c012c2ab495e8656
junggji <calrinar@gmail.com>
2023-11-03 10:41:22 +0900

    arrow function을 prop으로 전달

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import React from "react";
 import { Button } from "@chakra-ui/react";
 
-function MyComp({ to, someProp }) {
-  someProp();
-  return (
-    <>
-      <div>
-        <a href={to[0]}>link!!!</a>
-      </div>
-      <div>
-        <Button onClick={someProp}>버튼</Button>
-      </div>
-    </>
-  );
+function MyComp({ children, executeClick }) {
+  return <Button onClick={executeClick}>{children}</Button>;
 }
 
 function App(props) {
   function func1() {
-    console.log("func1 실행됨");
+    console.log("func1 실행");
   }
+  // ------------------------------- arrow function ~~~
+  let func2 = () => {
+    console.log("arrow function을 변수에 담아서 사용");
+  };
+
   return (
     <div>
-      <MyComp to={["https://www.naver.com"]} someProp={func1}></MyComp>
+      <MyComp executeClick={func1}>Button1</MyComp>
+      <MyComp executeClick={func2}>Button2</MyComp>
+      <MyComp
+        executeClick={() => {
+          console.log("arrow function을 변수에 담지 않고 사용!!!");
+        }}
+      >
+        Button3
+      </MyComp>
     </div>
   );
 }
