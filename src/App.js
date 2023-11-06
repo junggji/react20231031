6800b83db0fad47e40f8fed59a42b439607eb48f
junggji <calrinar@gmail.com>
2023-11-06 09:25:36 +0900

    state 사용 예제

--- src/App.js
+++ src/App.js
@@ -1,76 +1,23 @@
 import React, { useState } from "react";
-import {
-  Box,
-  Button,
-  Center,
-  FormControl,
-  FormHelperText,
-  FormLabel,
-  Input,
-} from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
 
 function App(props) {
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
-  const [submitting, setSubmitting] = useState(false);
+  const [text, setText] = useState("hello");
+  // react가 컴포넌트의 상태변화가 일어나는지 파악
+  // 상태를 통해서 변경을 해야한다.(그게 react활용이다)
 
-  function handleNameInput(e) {
-    setName(e.target.value);
-  }
+  console.log(text);
 
-  function handlePasswordInput(e) {
-    setPassword(e.target.value);
-  }
-
-  function handleEmailInput(e) {
-    setEmail(e.target.value);
-  }
-
-  function handleSubmit() {
-    setSubmitting(true);
-    // axios.post("/adduser", {
-    //   name, email, password
-    // }).then(response => console.log("성공할 때 해야하는 일"))
-    //   .catch(error => console.log("실패할 때 해야하는 일"))
-    //   .finally(() => console.log("꼭 해야하는 일"));
+  function handleButtonClick(e) {
+    // text 라는 state 값 변경
+    setText("greeting");
   }
 
   return (
-    <Center>
-      <Box w={"480px"}>
-        <FormControl>
-          <FormLabel>Name</FormLabel>
-          <Input type="text" value={name} onChange={handleNameInput} />
-          <FormHelperText>띄어쓰기 없이 입력해주세요</FormHelperText>
-        </FormControl>
-        <FormControl>
-          <FormLabel>Password</FormLabel>
-          <Input
-            type="password"
-            value={password}
-            onChange={handlePasswordInput}
-          />
-          <FormHelperText>
-            특수기호와 숫자를 하나 이상 작성해주세요.
-          </FormHelperText>
-        </FormControl>
-
-        <FormControl>
-          <FormLabel>Email</FormLabel>
-          <Input type="email" value={email} onChange={handleEmailInput} />
-          <FormHelperText>입력된 이메일은 중복될 수 없습니다.</FormHelperText>
-        </FormControl>
-
-        <Button
-          onClick={handleSubmit}
-          colorScheme="blue"
-          isLoading={submitting}
-        >
-          가입
-        </Button>
-      </Box>
-    </Center>
+    <div>
+      <Button onClick={handleButtonClick}>상태 변경!</Button>
+      <p>{text}</p>
+    </div>
   );
 }
 
