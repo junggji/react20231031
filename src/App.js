9fe26768e4911ec14d1fb4dcf6732d40cdc1e4db
junggji <calrinar@gmail.com>
2023-11-06 10:27:02 +0900

    hook 사용 조건 설명

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 
 function App(props) {
   // HOOK : use 로 시작하는 함수
   // hook은 컴포넌트 상단에 작성해야함(import사용하듯이 조건없이 써라)
   // 조건문이나, 반복문, 중첩된 함수안에 작성하면 안됨
 
-  if(true) {
+  if (true) {
     // hook 호출 코드 작성 불가
   }
 
-  for(let i = 0; i<3; i++) {
+  for (let i = 0; i < 3; i++) {
     // hook 호출 코드 작성 불가
   }
 
@@ -17,9 +17,7 @@ function App(props) {
     // hook 호출 코드 작성 불가
   }
 
-  return (
-    <div></div>
-  );
+  return <div></div>;
 }
 
 export default App;
