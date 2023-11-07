d313a79289e7c2d9154ca1a0fb091b74b48e7ed0
junggji <calrinar@gmail.com>
2023-11-07 14:14:47 +0900

    context value에 {state와 setState}를 넣기

--- src/App.js
+++ src/App.js
@@ -1,45 +1,42 @@
 import React, { createContext, useContext, useState } from "react";
 import { Button, Text } from "@chakra-ui/react";
 
-function CComp() {
+function AComp() {
   // 3. context 사용하기 : useContext(Context)
-  const message = useContext(MessageContext);
-  return <Text>받은 메세지 : {message}</Text>;
+  const { setMessage } = useContext(MessageContext);
+  return (
+    <Button onClick={() => setMessage("변경된 메세지")}>
+      메세지 변경하기!!
+    </Button>
+  );
 }
 
 function BComp() {
-  return <CComp />;
-}
-
-function AComp() {
-  return <BComp />;
+  // 3. context 사용하기 : useContext(Context)
+  const { message } = useContext(MessageContext);
+  return <Text>{message}</Text>;
 }
 
 function App(props) {
-  const [message, setMessage] = useState("");
-
-  // message state를 Ccomp에 전달 하기
-  // 1. context 만들기 : creatContext();
-  // component 밖에서 만들어야함
-  // 2. context에 state 넣기 (제일 조상 component에)
-  //    : <Context.Provider value={state}></Context.Provider>
-  // 3. tree안에 context 사용하기
+  const [message, setMessage] = useState("초기 상태 메세지");
 
   return (
     <div>
-      <Button onClick={() => setMessage("바꾼 메세지!!!")}>
-        메세지 바꾸기
-      </Button>
-      <MessageContext.Provider value={message}>
+      {/* 2. context에 value 넣기(객체, primitive타입 등등..) : Context.Provider value={state}><Context.Provider> */}
+      {/* 아래 value에 객체 이름과 값이 같으면, 값을 생략가능 */}
+      <MessageContext.Provider
+        value={{
+          message,
+          setMessage,
+        }}
+      >
         <AComp />
+        <BComp />
       </MessageContext.Provider>
     </div>
   );
 }
-
-// 1.context 만들기
-// context 이름은 대문자로 시작하고 ...Context로 끝남 (ex. LoginContext, MemberContext)
-// 초기값에 null을 넣는게 습관인듯하다
+// 1. context 만들기 : creatContext(null)
 const MessageContext = createContext(null);
 
 export default App;
