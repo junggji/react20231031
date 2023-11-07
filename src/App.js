dc5552a2f40a790a5b442b6c417a0b43ba49b631
junggji <calrinar@gmail.com>
2023-11-07 14:53:16 +0900

    component 밖의 값을 가능한 변경하지 않도록 할 것!

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-import React, { createContext, useState } from "react";
-import { AComp } from "./AComp";
-import { BComp } from "./BComp";
+import React from "react";
+
+function MyComp() {
+  console.log("good?");
+  return <div>hello</div>;
+}
 
 function App(props) {
-  const [message, setMessage] = useState("초기 상태 메세지");
-
   return (
     <div>
-      {/* 2. context에 value 넣기(객체, primitive타입 등등..) : Context.Provider value={state}><Context.Provider> */}
-      {/* 아래 value에 객체 이름과 값이 같으면, 값을 생략가능 */}
-      <MessageContext.Provider
-        value={{
-          message,
-          setMessage,
-        }}
-      >
-        <AComp />
-        <BComp />
-      </MessageContext.Provider>
+      <MyComp />
     </div>
   );
 }
-// 1. context 만들기 : creatContext(null)
-export const MessageContext = createContext(null);
 
 export default App;
